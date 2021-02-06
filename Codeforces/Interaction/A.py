3eeb48d0b23cdb4a5786457b0b36a53808a4c0e6
Tristan Lee <tlee10@stuy.edu>
2021-02-06 17:43:13 -0500

    CF Interactive Problem

--- Codeforces/Interaction/A.py
+++ Codeforces/Interaction/A.py
@@ -1,18 +1,11 @@
-import sys
-from math import ceil
 l=1
 r=1000000
-while l<r-1:
-    mid=(l+r)//2
-    print(mid,l,r)
-    sys.stdout.flush()
+while l!=r:
+    mid=(l+r+1)//2
+    print(mid)
     s=input()
     if s=='<':
         r=mid-1
     else:
         l=mid
-print(r)
-if input()=='<':
-    print('!',l)
-else:
-    print('!',r)
+print('!',l)
