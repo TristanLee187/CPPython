4653b89e7033ac5a2600fcafd245fe71648afccc
Tristan Lee <tlee10@stuy.edu>
2021-02-11 14:07:37 -0500

    add bool array vs prime list modes

--- MyLib/MyPrimeSieve.py
+++ MyLib/MyPrimeSieve.py
@@ -1,13 +1,17 @@
-def sieve(n):
+def sieve(n,choice):
     ans=(n+1)*[True]
     for i in range(2,int(n**0.5)+1):
         if ans[i]:
             for j in range(2*i,n+1,i):
                 ans[j]=False
+    # ans is the bool array; ans[i] is True if i is prime, false otherwise
+    if choice:
+        return ans
     res=[]
     for i in range(2,n+1):
         if ans[i]:
             res.append(i)
+    # res is the list of primes
     return res
 # from time import time
 # t1=time()
