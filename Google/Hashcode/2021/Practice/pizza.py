e1b755803e5129d853f6b3e113358c6fa64d24b1
TristanLee <tlee10@stuy.edu>
2021-02-17 15:27:21 -0500

    Hashcode stuff, update template

--- Google/Hashcode/2021/Practice/pizza.py
+++ Google/Hashcode/2021/Practice/pizza.py
@@ -1,14 +1,29 @@
-prob="PROB_NAME"
+prob="a_example"
 
-file=open(prob+".in")
-
+file=open(prob)
 
 rn=lambda:int(file.readline())
 rns=lambda:map(int,file.readline().split())
 rl=lambda:list(map(int,file.readline().split()))
 rs=lambda:file.readline()
-yn=lambda x:print('Yes') if x else print('No')
-YN=lambda x:print('YES') if x else print('NO')
-pl=lambda l:print(' '.join(list(map(str,l))))
+pl=lambda l:file.write(' '.join(list(map(str,l))))
 
+#
+#Read input
 m,one,two,three=rns()
+pizzas={}
+for i in range(m):
+    s=rs().split()
+    pizzas[i]=s[1:]
+#
+file.close()
+
+#CODE
+ans=1
+#
+
+file=open(prob+".out","w")
+file.write(str(ans)+'\n')
+for i in range(ans):
+    file.write('2 0 1\n')
+file.close()
--- Google/Template.py
+++ Google/Template.py
@@ -1,7 +1,14 @@
-for _ in range(int(input())):
+rn=lambda:int(input())
+rns=lambda:map(int,input().split())
+rl=lambda:list(map(int,input().split()))
+rs=lambda:input()
+yn=lambda x:print('Yes') if x else print('No')
+YN=lambda x:print('YES') if x else print('NO')
+mod=lambda x:x%(10**9+7)
 
+for _ in range(rn()):
+    ans=0
     
     
 
     print('Case #' + str(_+1)+': ' + str(ans))
-        
