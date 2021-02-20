0fc8809e9b4e1c7b7e78a315210acf0db9f3e2f5
TristanLee <tlee10@stuy.edu>
2021-02-20 13:05:31 -0500

    practice

--- Hackercup/2020QA/ProblemC.py
+++ Hackercup/2020QA/ProblemC.py
@@ -1,38 +1,14 @@
-import bisect
+rn=lambda:int(input())
+rns=lambda:map(int,input().split())
+rl=lambda:list(map(int,input().split()))
+rs=lambda:input()
+yn=lambda x:print('Yes') if x else print('No')
+YN=lambda x:print('YES') if x else print('NO')
+mod=lambda x:x%(10**9+7)
 
-file=open('ans.txt', 'w')
-file=open('timber_sample_input.txt', 'r')
-t=int(file.readline())
-for _ in range(t):
-    n=int(file.readline())
-    pos=[]
-    his=[]
+for _ in range(rn()):
+    n=rn()
+    l=rl()
     for i in range(n):
-        p,h=map(int,file.readline().split())
-        pos.append(p)
-        his.append(h)
-    tpos=pos
-    tpos=sorted(pos)
-    this=[]
-    this.extend(n*[0])
-    for i in range(n):
-        j=bisect.bisect_left(tpos,pos[i])
-        this[j]=his[i]
-    ans=-float('inf')
-    point=tpos[0]
-    curr=0
-    for i in range(n):
-        if tpos[i]==point or tpos[i]-this[i]==point:
-            curr+=this[i]
-            point+=this[i]
-        else:
-            ans=max(ans,curr)
-            curr=this[i]
-            point=tpos[i]+this[i]
-    ans=max(curr,ans)
-    print(tpos)
-    print(this)
-    print('Case #'+str(_+1)+': ' + str(ans))
-        
-    
-    
+        l.append(rl())
+    l.sort()
