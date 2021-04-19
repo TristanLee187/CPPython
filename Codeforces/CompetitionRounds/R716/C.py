b95df6ced6bfc9cf4ca728a7d9d7b04ec1d96858
TristanLee <tlee10@stuy.edu>
2021-04-19 12:37:26 -0400

    CF R716 fix C

--- Codeforces/CompetitionRounds/R716/C.py
+++ Codeforces/CompetitionRounds/R716/C.py
@@ -6,25 +6,20 @@ rl=lambda:list(map(int,input().split()))
 rs=lambda:input()
 YN=lambda x:print('YES') if x else print('NO')
 mod=10**9+7
+from math import gcd
 
 n=rn()
-ans=1
-pans=[1]
 nums=[]
-for i in range(2,n):
-    try:
-        inv=pow(i,-1,n)
-        nums.append((i,inv))
-    except:
-        ans+=0
+for i in range(1,n):
+    if gcd(i,n)==1:
+        nums.append(i)
 prod=1
-for i in nums:
-    prod*=i[0]
+for num in nums:
+    prod*=num
+    prod%=n
+if prod!=1:
+    nums.remove(prod)
+ans=len(nums)
 
-for i in nums:
-    if (prod/i[0])%i[1]==n%i[0]:
-        ans+=1
-        pans.append(i[0])
-print(nums)
 print(ans)
-print(*pans)
+print(*nums)
