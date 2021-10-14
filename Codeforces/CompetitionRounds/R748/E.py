4db597db53815fde155ddcc7828143bfdde8cc2c
Tristan Lee <tlee10@stuy.edu>
2021-10-14 11:52:12 -0400

    fix R748 E

--- Codeforces/CompetitionRounds/R748/E.py
+++ Codeforces/CompetitionRounds/R748/E.py
@@ -11,34 +11,38 @@ mod=10**9+7
 for _ in range(rn()):
     s=rs()
     n,k=rns()
-    tree=defaultdict(set)
-    for __ in range(n-1):
-        u,v=rns()
-        tree[u].add(v)
-        tree[v].add(u)
-    # print(tree)
-    ans=defaultdict(int)
-    nodes=n
-    depth=1
-    leaves = [node for node in tree if len(tree[node]) <= 1]
-    while nodes>0:
-        # print(depth, ans)
-        new_leaves = []
-        for leaf in leaves:
-            ans[leaf]=max(ans[leaf],depth)
-            if tree[leaf]:
-                adj = list(tree[leaf])[0]
-                tree[adj].remove(leaf)
-                if len(tree[adj])==1:
-                    new_leaves.append(adj)
-            tree.__delitem__(leaf)
-        depth+=1
-        nodes-=len(leaves)
-        leaves = new_leaves.copy()
-    # print(ans)
-    amts = Counter(ans.values())
-    # print(amts)
-    pans=n
-    for i in range(k):
-        pans-=amts[i+1]
-    print(pans)
+    if n==1:
+        print(0)
+    else:
+        tree=defaultdict(set)
+        for __ in range(n-1):
+            u,v=rns()
+            tree[u].add(v)
+            tree[v].add(u)
+        # print(tree)
+        ans=defaultdict(int)
+        nodes=n
+        depth=1
+        leaves = [node for node in tree if len(tree[node]) <= 1]
+        while nodes>0:
+            # print(depth, ans)
+            new_leaves = []
+            for leaf in leaves:
+                ans[leaf]=depth
+                if tree[leaf]:
+                    adj = tree[leaf].pop()
+                    tree[adj].remove(leaf)
+                    if len(tree[adj])==1:
+                        new_leaves.append(adj)
+                tree.pop(leaf)
+            depth+=1
+            nodes-=len(leaves)
+            leaves = new_leaves
+        # print(ans)
+        # print(amts)
+        pans=n
+        # print(ans.values())
+        for num in ans.values():
+            if num<=k:
+                pans-=1
+        print(pans)
