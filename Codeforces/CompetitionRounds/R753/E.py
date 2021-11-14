1ab288f712c07670106801f13043409d4f9326d7
TristanLee <tlee10@stuy.edu>
2021-11-13 19:53:31 -0500

    practice

--- Codeforces/CompetitionRounds/R753/E.py
+++ Codeforces/CompetitionRounds/R753/E.py
@@ -9,3 +9,23 @@ ceil_div=lambda a,b:-(-a//b)
 mod=10**9+7
 
 for _ in range(rn()):
+    n,m=rns()
+    s=rs()
+    lr=[0,0]
+    ud=[0,0]
+    currx=0
+    curry=0
+    for i in s:
+        if i=='R':
+            currx+=1
+            lr[1] = max(lr[1], currx)
+        elif i=='L':
+            currx-=1
+            lr[0] = min(lr[0], currx)
+        elif i=='U':
+            curry+=1
+            ud[0] = min(ud[0], curry)
+        else:
+            curry-=1
+            ud[1] = max(ud[1], curry)
+            ud[0] = min(ud[0], curry)
