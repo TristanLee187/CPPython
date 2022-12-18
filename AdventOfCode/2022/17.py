edccd640cdc85c5af12f1bc28c927ff0f504f90b
Tristan Lee <tristanlee73@gmail.com>
2022-12-18 18:00:39 -0500

    AoC Day 17 part 2

--- AdventOfCode/2022/17.py
+++ AdventOfCode/2022/17.py
@@ -46,8 +46,48 @@ def part_1():
 
 # part 2
 def part_2():
-    ans = 0
-
+    def sim(cycle_len):
+        gas = lines[0]
+        gas_p = 0
+        rocks = [['####'], ['.#.', '###', '.#.'], ['..#', '..#', '###'], ['#', '#', '#', '#'], ['##', '##']]
+        shaft = []
+        for i in range(4*cycle_len):
+            shaft.append(['.' for j in range(7)])
+        shaft.append(['#' for j in range(7)])
+        curr_depth = len(shaft) - 1
+        for i in range(cycle_len):
+            rock = rocks[i % 5][::-1]
+            coors = [curr_depth - 4, 2]
+            while True:
+                prev_col = coors[1]
+                if gas[gas_p] == '>':
+                    coors[1] = min(coors[1] + 1, 7 - len(rock[0]))
+                else:
+                    coors[1] = max(0, coors[1] - 1)
+                gas_p += 1
+                gas_p %= len(gas)
+                flag = any([shaft[coors[0] - r][coors[1] + c] == rock[r][c] == '#'
+                            for r in range(len(rock)) for c in range(len(rock[0]))])
+                if flag:
+                    coors[1] = prev_col
+                coors[0] += 1
+                flag = any([shaft[coors[0] - r][coors[1] + c] == rock[r][c] == '#'
+                            for r in range(len(rock)) for c in range(len(rock[0]))])
+                if flag:
+                    coors[0] -= 1
+                    break
+            for r in range(len(rock)):
+                for c in range(len(rock[0])):
+                    if rock[r][c] == '#':
+                        shaft[coors[0] - r][coors[1] + c] = rock[r][c]
+            curr_depth = min(curr_depth, coors[0] - len(rock) + 1)
+        ans = len(shaft) - curr_depth - 1
+        return ans
+    # constants found manually
+    initial = sim(1723)
+    e_change = sim(1723 + 1715) - initial
+    l_change = sim(1723 + 1722) - initial
+    ans = initial + 583090377 * e_change + l_change
     return ans
 
 
